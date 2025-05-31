728378ca764accef3317f193abd0f7df04a2c05d
r1cardohj <2413302357@qq.com>
2025-06-01 02:55:01 +0800

    feat: change backend, from leveldb to badger

--- stock.go
+++ stock.go
@@ -1,61 +1,98 @@
 package main
 
 import (
-	"sync"
-	"errors"
 	"encoding/binary"
-	"github.com/syndtr/goleveldb/leveldb"
+	"errors"
+	"sync"
+
+	"github.com/dgraph-io/badger"
 )
 
-// a wrapper for leveldb
-type LevelDBQueue struct {
-	db       *leveldb.DB
+type Queue struct {
+	db       *badger.DB
 	path     string
 	prefix   []byte
 	mu       sync.Mutex
-	enqueued uint64 //a counter for item in queue
-	dequeued uint64 // item leave queue
+	enqueued uint64
+	dequeued uint64
 }
 
-
-func NewQueue(path string, prefix []byte) (*LevelDBQueue, error) {
-	db, err := leveldb.OpenFile(path, nil)
+func NewQueue(path string, prefix []byte) (*Queue, error) {
+	opts := badger.DefaultOptions(path)
+	db, err := badger.Open(opts)
 	if err != nil {
 		return nil, err
 	}
 
-	return &LevelDBQueue{
-		db: db,
-		path: path,
+	queue := &Queue{
+		db:     db,
+		path:   path,
 		prefix: prefix,
-	}, nil
+	}
+
+	err = queue.restoreCounters()
+	if err != nil {
+		return nil, err
+	}
+
+	return queue, nil
 }
 
+func (q *Queue) restoreCounters() error {
+	return q.db.View(func(txn *badger.Txn) error {
+		// recover enqueued counter
+		enqKey := append(q.prefix, []byte("_meta_enqueued")...)
+		item, err := txn.Get(enqKey)
+		if err == nil {
+			val, err := item.ValueCopy(nil)
+			if err == nil && len(val) == 8 {
+				q.enqueued = binary.BigEndian.Uint64(val)
+			}
+		}
 
-func (q *LevelDBQueue) Close() error {
+		// recover dequeued counter
+		deqKey := append(q.prefix, []byte("_meta_dequeued")...)
+		item, err = txn.Get(deqKey)
+		if err == nil {
+			val, err := item.ValueCopy(nil)
+			if err == nil && len(val) == 8 {
+				q.dequeued = binary.BigEndian.Uint64(val)
+			}
+		}
+
+		return nil
+	})
+}
+
+func (q *Queue) Close() error {
 	return q.db.Close()
 }
 
-
-func (q *LevelDBQueue) generateKey() []byte {
+func (q *Queue) generateKey() []byte {
 	q.mu.Lock()
 	defer q.mu.Unlock()
 	q.enqueued++
 
-	// uint64 is 8 byte
-	key := make([]byte, len(q.prefix) + 8)
+	_ = q.db.Update(func(txn *badger.Txn) error {
+		key := append(q.prefix, []byte("_meta_enqueued")...)
+		val := make([]byte, 8)
+		binary.BigEndian.PutUint64(val, q.enqueued)
+		return txn.Set(key, val)
+	})
+
+	key := make([]byte, len(q.prefix)+8)
 	copy(key, q.prefix)
-	// append to prefix, if prefix is queue_, full key is queue_\0x...x8
 	binary.BigEndian.PutUint64(key[len(q.prefix):], q.enqueued)
 	return key
 }
 
-func (q *LevelDBQueue) Enqueue(value []byte) error {
-	return q.db.Put(q.generateKey(), value, nil)
+func (q *Queue) Enqueue(value []byte) error {
+	return q.db.Update(func(txn *badger.Txn) error {
+		return txn.Set(q.generateKey(), value)
+	})
 }
 
-
-func (q *LevelDBQueue) Dequeue() ([]byte, error) {
+func (q *Queue) Dequeue() ([]byte, error) {
 	q.mu.Lock()
 	defer q.mu.Unlock()
 
@@ -63,25 +100,42 @@ func (q *LevelDBQueue) Dequeue() ([]byte, error) {
 		return nil, errors.New("queue is empty")
 	}
 
-
-	key := make([]byte, len(q.prefix) + 8)
+	nextID := q.dequeued + 1
+	key := make([]byte, len(q.prefix)+8)
 	copy(key, q.prefix)
-	binary.BigEndian.PutUint64(key[len(q.prefix):], q.dequeued+1)
+	binary.BigEndian.PutUint64(key[len(q.prefix):], nextID)
 
-	value, err := q.db.Get(key, nil)
+	var value []byte
+	err := q.db.Update(func(txn *badger.Txn) error {
+		item, err := txn.Get(key)
+		if err != nil {
+			return err
+		}
+
+		value, err = item.ValueCopy(nil)
+		if err != nil {
+			return err
+		}
+
+		if err := txn.Delete(key); err != nil {
+			return err
+		}
+
+		q.dequeued = nextID
+		metaKey := append(q.prefix, []byte("_meta_dequeued")...)
+		val := make([]byte, 8)
+		binary.BigEndian.PutUint64(val, q.dequeued)
+		return txn.Set(metaKey, val)
+	})
+
 	if err != nil {
 		return nil, err
 	}
 
-	if err := q.db.Delete(key, nil); err != nil {
-		return nil, err
-	}
-
-	q.dequeued++
 	return value, nil
 }
 
-func (q *LevelDBQueue) Size() uint64 {
+func (q *Queue) Size() uint64 {
 	q.mu.Lock()
 	defer q.mu.Unlock()
 	return q.enqueued - q.dequeued
